9599662f61c5cc09c75da7d1d82cb6291fe809a9
NagyAdrienn <adri.nagy.91@gmail.com>
2021-08-03 00:48:34 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var target = document.getElementById('target');
+var titles = [
+    'Title 1',
+    'Title 2',
+    'Title 3',
+    'Title 4',
+    'Title 5'
+];
+
+function newTitle () {
+    var i = (Math.random() * titles.length) | 0;
+    target.innerText = titles[i];
+}
+
+newTitle();
+
+
 // toggle
 var show = function (elem) {
 	elem.style.display = 'block';
